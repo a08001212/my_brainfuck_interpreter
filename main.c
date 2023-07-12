83c006ccdfd5d886305ee6d2b6b98d30f0bdf473
a08001212 <a08001212@gmail.com>
2023-07-12 16:07:06 +0800

    let it can run with more them one brainfuck file.

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Interpreter.h"
-int main(int argv, char **argc) {
-    if(argv < 2){
+int main(int argc, char **argv) {
+    if(argc < 2){
         printf("Please choose a file\n");
         exit(1);
     }
-    Interpreter(argc[1]);
+    for(int i = 1; i<argv; ++i){
+        Interpreter(argv[i]);
+    }
+
     return 0;
 }
